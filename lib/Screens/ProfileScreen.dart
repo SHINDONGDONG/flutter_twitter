5533d70e693b27b25ac268d3a5f19fdbc4fcbd19
homerjay <homerjay@naver.com>
2021-04-07 06:55:36 +0900

    database service, follow method

--- lib/Screens/ProfileScreen.dart
+++ lib/Screens/ProfileScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_twitter/Service/DatabaseServicese.dart';
 
 class ProfileScreen extends StatefulWidget {
   final String currentUserId;
@@ -14,8 +15,38 @@ class _ProfileScreenState extends State<ProfileScreen> {
   int _followersCount = 0;
   int _followingCount = 0;
 
+  //팔로워수 카운트하는 메소드
   getFollowersCount()async{
-      
+    int followersCount  = await DatabaseServicese.followersNum(widget.visitedUserId);
+    //mounted란 dispose등의 상태관리가 끝난경우 false가 된다. 살아있는경우 true
+    if(mounted){
+      setState(() {
+        //데이터베이스의 followers의 비짓유저아이디를 followersccunt에 넣는다.
+        _followersCount = followersCount;
+      });
+    }
+  }
+
+  //팔로잉수 카운트하는 메소드
+  getFollowingCount()async{
+    int followingCount  = await DatabaseServicese.followingNum(widget.visitedUserId);
+    //mounted란 dispose등의 상태관리가 끝난경우 false가 된다. 살아있는경우 true
+    if(mounted){
+      setState(() {
+        //데이터베이스의 followers의 비짓유저아이디를 followersccunt에 넣는다.
+        _followingCount = followingCount;
+      });
+    }
+
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //initstate로 초기 실행시 1회 실행하게만듬.
+    getFollowingCount();
+    getFollowersCount();
   }
 
   @override
---
+++ lib/Service/DatabaseServicese.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_twitter/constants.dart';
+
+class DatabaseServicese {
+  static Future<int> followersNum(String userId) async {
+    //쿼리문선언
+    //followersSnapshot에                  //followersRef 는 팔로잉 콜렉션을 담고있다 그중 userid를 찾아 겟한다
+    QuerySnapshot followersSnapshot = await followersRef.doc(userId).collection("userFollowers").get();
+    return followersSnapshot.docs.length; //팔로잉 스냅샷 독 랭쓰를 (레코드수) 리턴
+  }
+
+  static Future<int> followingNum(String userId) async {
+    //쿼리문선언
+    //followersSnapshot에                  //followersRef 는 팔로잉 콜렉션을 담고있다 그중 userid를 찾아 겟한다
+    QuerySnapshot followingSnapshot = await followingsRef.doc(userId).collection("userFollowing").get();
+    return followingSnapshot.docs.length; //팔로잉 스냅샷 독 랭쓰를 (레코드수) 리턴
+  }
+}
